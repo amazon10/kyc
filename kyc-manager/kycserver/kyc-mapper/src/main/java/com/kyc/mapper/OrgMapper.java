7a1e17a0ebfb50733193f38bda25daec779fa76d
zhaochaofeng <zhaochaofeng@chinamobile.com>
2020-08-28 22:53:34 +0800

    删除多余的类

---
+++ kyc-manager/kycserver/kyc-mapper/src/main/java/com/kyc/mapper/OrgMapper.java
@@ -0,0 +1,9 @@
+package com.kyc.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kyc.model.Org;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrgMapper extends BaseMapper<Org> {
+}
--- kyc-manager/kycserver/kyc-web/src/test/java/com/kyc/KycApplicationTests.java
+++ kyc-manager/kycserver/kyc-web/src/test/java/com/kyc/KycApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class VhrApplicationTests {
+public class KycApplicationTests {
 
     @Test
     public void contextLoads() {
