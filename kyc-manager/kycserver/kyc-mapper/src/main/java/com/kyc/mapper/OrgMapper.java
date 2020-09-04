2c834583b4a8d69f469aaf9535b5cd8da2e6e20a
amazon10 <1048357761@qq.com>
2020-09-04 22:38:36 +0800

    Delete OrgMapper.java

--- kyc-manager/kycserver/kyc-mapper/src/main/java/com/kyc/mapper/OrgMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kyc.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kyc.model.Org;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrgMapper extends BaseMapper<Org> {
-}
